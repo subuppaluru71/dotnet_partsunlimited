c8b88a2de2e159c299b62b2e086cc05c02a8044f
suppaluru-us <s.uppaluru@outlook.com>
2021-06-12 18:59:04 +0530

    Added partsunlimited project

---
+++ src/PartsUnlimited.API/Controllers/V2SpecialsController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PartsUnlimited.Models;
+
+namespace PartsUnlimited.API.Controllers
+{
+    [Route("api/v2/specials")]
+    public class V2SpecialsController : Controller
+    {
+        [HttpGet("{id}")]
+        public IEnumerable<Product> GetSpecialsByUserId(int id)
+        {
+            var productsOnSpecial = new List<Product>
+            {
+                new Product
+                {
+                    SkuNumber = "OIL-0001",
+                    Title = "Filter Set V2",
+                    CategoryId = 5,
+                    Price = 28.99M,
+                    Discount="30%",
+                    SalePrice = 28.99M,
+                    ProductArtUrl = "https://raw.githubusercontent.com/Microsoft/PartsUnlimited/master/src/PartsUnlimitedWebsite/images/product_oil_filters.jpg",
+                    ProductDetails = "{ \"Filter Type\" : \"Canister and Cartridge\", \"Thread Size\" : \"0.75-16 in.\", \"Anti-Drainback Valve\" : \"Yes\"}",
+                    Description = "Ensure that your vehicle's engine has a longer life with our new filter set. Trapping more dirt to ensure old freely circulates through your engine.",
+                    Inventory = 3,
+                    LeadTime = 0,
+                    RecommendationId = 16
+                },
+                new Product
+                {
+                    SkuNumber = "OIL-0002",
+                    Title = "Oil and Filter Combo V2",
+                    CategoryId = 5,
+                    Price = 34.49M,
+                    SalePrice = 34.49M,
+                    Discount="30%",
+                    ProductArtUrl = "https://raw.githubusercontent.com/Microsoft/PartsUnlimited/master/src/PartsUnlimitedWebsite/images/product_oil_oil-filter-combo.jpg",
+                    ProductDetails = "{ \"Filter Type\" : \"Canister\", \"Thread Size\" : \"0.75-16 in.\", \"Anti-Drainback Valve\" : \"Yes\", \"Size\" : \"1.1 gal.\", \"Synthetic\" : \"No\" }",
+                    Description = "This Oil and Oil Filter combo is suitable for all types of passenger and light commercial vehicles. Providing affordable performance through excellent lubrication and breakdown resistance.",
+                    Inventory = 5,
+                    LeadTime = 0,
+                    RecommendationId = 17
+                },
+                new Product
+                {
+                    SkuNumber = "OIL-0003",
+                    Title = "Synthetic Engine Oil V2",
+                    CategoryId = 5,
+                    Price = 36.49M,
+                    SalePrice = 36.49M,
+                    Discount="30%",
+                    ProductArtUrl = "https://raw.githubusercontent.com/Microsoft/PartsUnlimited/master/src/PartsUnlimitedWebsite/images/product_oil_premium-oil.jpg",
+                    ProductDetails = "{ \"Size\" :  \"1.1 Gal.\" , \"Synthetic\" : \"Yes\"}",
+                    Description = "This Oil is designed to reduce sludge deposits and metal friction throughout your cars engine. Provides performance no matter the condition or temperature.",
+                    Inventory = 11,
+                    LeadTime = 0,
+                    RecommendationId = 18
+                }
+            };
+            return productsOnSpecial;
+        }
+    }
+}
